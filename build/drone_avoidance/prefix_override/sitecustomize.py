971c76e894df84a2fccc3f48e8b17d7a4fca3af6
tugberk.varoglu <tugberkvaroglu03@gmail.com>
2024-11-07 22:17:51 +0300

    build

--- build/drone_avoidance/prefix_override/sitecustomize.py
+++ build/drone_avoidance/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tugberk/Desktop/ROS-test/install/drone_avoidance'
+    sys.prefix = sys.exec_prefix = '/home/tugberk/Desktop/Projects/ROS-test/install/drone_avoidance'
